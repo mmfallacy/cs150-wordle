7446a00eb8718cff21fcda249e64b15390e51c49
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2022-10-03 04:09:21 +0800

    feat: colors and routing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /// <reference path="utils.ts"/>
 /// <reference path="Start.page.ts"/>
 /// <reference path="store.ts"/>
+/// <reference path="colors.ts"/>
 
 const root = document.getElementById("root");
 const useGlobalStore = createStore();
@@ -9,4 +10,16 @@ const { word } = useGlobalStore();
 
 word.sub((value) => console.log("WORD:", value));
 
-root?.renderSubtree(Start());
+const h1 = styled("h1")`
+    font-size: 10rem;
+    color: red;
+`;
+
+word.sub((value) => (h1.textContent = value));
+
+word.sub(
+    (value) => root?.renderSubtree(value ? KeyboardLayout() : Start()),
+    true
+);
+
+word.pub("wroth");
